a10adc3009d47245f1d0fb0569042dad796e4678
Edimar Miranda Jr <edimarbmjunior@gmail.com>
2018-04-21 00:06:31 -0300

    criacao da operacao de instanciacao do banco

--- src/main/java/com/curso/mc/ModelagemConcetualApplication.java
+++ src/main/java/com/curso/mc/ModelagemConcetualApplication.java
@@ -1,12 +1,31 @@
 package com.curso.mc;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemConcetualApplication {
+import com.curso.mc.domain.Categoria;
+import com.curso.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemConcetualApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConcetualApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
